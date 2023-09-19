0a86e5f88769246bed43fae4f1b69a87ccddead7
Carlos Moraes <carloshrmoraes@gmail.com>
2023-09-19 15:24:06 -0300

    fixing bug on season getting stuck

--- src/components/APIHelper.tsx
+++ src/components/APIHelper.tsx
@@ -1,25 +1,38 @@
 import Anime from "../classes/Anime";
 
-function parseAnimeData(data: any[]): Anime[]
-{
+function parseAnimeData(data: any[]): Anime[] {
     return data.flatMap(a => new Anime(a.title, a.images.jpg.image_url, a.mal_id, a.link));
 }
 
 async function fetchFromAPI() {
     const localCache = localStorage.getItem("rawAnimeData");
-    if (localCache === null)
-    {
-        const apiCall = await fetch(`https://api.jikan.moe/v4/seasons/now`);
-        const animeData = await apiCall.json();
-        localStorage.setItem("rawAnimeData", JSON.stringify(animeData.data));
-        console.log("api call");        
-        console.log(animeData);        
-        return parseAnimeData(animeData.data);
-    } else
-    {
-        console.log("local data");     
-        return parseAnimeData(JSON.parse(localCache));
+    let anime: any = undefined;
+    if (localCache === null) {
+        anime = callAPI();
+    } else {
+        const lastFetch = localStorage.getItem("rawAnimeData_age");
+        if (lastFetch !== null) {
+            const fourMonths = 7786800000;
+            const age = new Date().valueOf() - new Date(lastFetch).valueOf();
+            if (age >= fourMonths) {
+                anime = callAPI();
+            } else {
+                anime = JSON.parse(localCache);
+            }
+        } else {
+            anime = callAPI();
+            localStorage.setItem("rawAnimeData_age", new Date().valueOf().toString());
+        }
     }
+    return parseAnimeData(anime);
 }
 
-export default fetchFromAPI
+async function callAPI() {
+    const apiCall = await fetch(`https://api.jikan.moe/v4/seasons/now`);
+    const animeData = await apiCall.json();
+    localStorage.setItem("rawAnimeData", JSON.stringify(animeData.data));
+    // console.log(animeData.data);
+    return animeData.data;
+}
+
+export default fetchFromAPI;
